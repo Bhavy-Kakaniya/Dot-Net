67707f443bedcfd802b6e15b7ae2d99384cea519
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-07-08 16:06:11 +0530

    initialise project

--- Labs/Lab3/obj/Debug/net10.0/Lab3.AssemblyInfo.cs
+++ Labs/Lab3/obj/Debug/net10.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af6cab8a7779f05ab955d6e561668a97afdba79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ff95d08ef699ab9c55b3ffeed93da7e8b6f35f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Project/StudentProjectManagementSystem/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
