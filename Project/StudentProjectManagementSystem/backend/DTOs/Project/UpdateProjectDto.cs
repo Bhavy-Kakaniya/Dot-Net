4c9375d0e2697b27385329b98b6bc63ac6da609d
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-07-18 11:36:10 +0530

    udate project dto completed

---
+++ Project/StudentProjectManagementSystem/backend/DTOs/Project/UpdateProjectDto.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentProjectManagementSystem.DTOs.Project
+{
+    public class UpdateProjectDto
+    {
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Technology { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int CreatedByUserId { get; set; }
+
+    }
+}
