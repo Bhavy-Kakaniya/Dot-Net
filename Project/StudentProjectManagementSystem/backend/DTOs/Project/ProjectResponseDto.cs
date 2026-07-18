234a69f772f9af2beb7d9a43bb3bdb372076c929
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-07-18 11:40:56 +0530

    project response dto completed

---
+++ Project/StudentProjectManagementSystem/backend/DTOs/Project/ProjectResponseDto.cs
@@ -0,0 +1,19 @@
+namespace StudentProjectManagementSystem.DTOs.Project
+{
+    public class ProjectDto
+    {
+        public int ProjectId { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Technology { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int CreatedByUserId { get; set; }
+
+    }
+}
