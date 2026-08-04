4eb939cc320c66b60d8e189d29fdc6ade4061e69
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-04 20:06:05 +0530

    project model updated

--- Project/StudentProjectManagementSystem/backend/Models/Project.cs
+++ Project/StudentProjectManagementSystem/backend/Models/Project.cs
@@ -9,26 +9,11 @@ public class Project
 
     [Required]
     [StringLength(150)]
-    public string Title { get; set; } = string.Empty;
+    public string ProjectTitle { get; set; } = string.Empty;
 
     [Required]
     [StringLength(1000)]
     public string Description { get; set; } = string.Empty;
 
-    [Required]
-    [StringLength(100)]
-    public string Technology { get; set; } = string.Empty;
-
-    [DataType(DataType.Date)]
-    public DateTime StartDate { get; set; }
-
-    [DataType(DataType.Date)]
-    public DateTime EndDate { get; set; }
-
-    [Required]
-    public int CreatedByUserId { get; set; }
-
-    public User? CreatedByUser { get; set; }
-
-    public ICollection<ProjectTask> ProjectTasks { get; set; } = new List<ProjectTask>();
+    public ICollection<ProjectAllocation> ProjectAllocations { get; set; } = new List<ProjectAllocation>();
 }
