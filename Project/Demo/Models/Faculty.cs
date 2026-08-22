164f057ac4a879bfcd82002fe15c6f856f1ec821
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-22 18:11:09 +0530

    Faculty model update

--- Project/Demo/Models/Faculty.cs
+++ Project/Demo/Models/Faculty.cs
@@ -6,5 +6,6 @@ public class Faculty
     public int UserId { get; set; }
     public string department { get; set; } = string.Empty;
     public int salary { get; set; }
+    public User User { get; set; } = null!;
     public ICollection<Student> Students { get; set; } = new List<Student>();
 }
