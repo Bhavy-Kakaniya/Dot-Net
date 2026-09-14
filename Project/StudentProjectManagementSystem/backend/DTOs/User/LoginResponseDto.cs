f29e47c829a17d4e8d0f1b5745d9302e96cd4ff7
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-09-14 23:09:09 +0530

    Add Login response dto

---
+++ Project/StudentProjectManagementSystem/backend/DTOs/User/LoginResponseDto.cs
@@ -0,0 +1,12 @@
+namespace StudentProjectManagementSystem.DTOs.User
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserType { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
